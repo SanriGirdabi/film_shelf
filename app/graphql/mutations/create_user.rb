49c38d10a5b149433b755071450ba8c8c296150f
Sercan Uygur <s.uygur@azerion.com>
2021-10-06 16:04:22 +0300

    Create own mutation file for user creation

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,17 @@
+class Mutations::CreateUser < Mutations::BaseMutation
+
+  argument :email, String, required: true
+  argument :password, String, required: true
+
+  field :user, Types::UserType, null: true
+
+  def resolve(email:, password:)
+    user = User.new(email: email, password: BCrypt::Password.create(password))
+
+    if user.save
+      user
+    else
+      user.errors.full_messages
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,20 +1,5 @@
-require 'bcrypt'
-
 module Types
   class MutationType < Types::BaseObject
-    field :create_user, UserType, null: true do
-      argument :email, String, required: true
-      argument :password, String, required: true
-    end
-
-    def create_user(email:, password:)
-      user = User.new(email: email, password: BCrypt::Password.create(password))
-
-      if user.save
-        user
-      else
-        user.errors.full_messages
-      end
-    end
+    field :create_user, type: Types::UserType, mutation: Mutations::CreateUser
   end
 end
